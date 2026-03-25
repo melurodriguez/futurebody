29501878916743d6eb4417b83bc30ec897f168e9
m3lb4 <melurod2003@gmail.com>
2026-03-25 15:47:05 -0300

    Limpieza: agrego docker, backend y elimino app.py, ignoro .venv

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://futurebody_user:FutureBodyUser2026@localhost/db"
+dotenv=load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST=os.getenv("DB_HOST")
+DB_NAME=os.getenv("DB_NAME")
+DB_PORT=os.getenv("DB_PORT")
+
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True) #false a produccion
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/schemas/usuarios_schema.py
+++ backend/schemas/usuarios_schema.py
@@ -6,14 +6,14 @@ class RolEnum(str, Enum):
     cliente = "cliente"
     profesional = "profesional"
 
-# 🔹 Crear usuario
+
 class UsuarioCreate(BaseModel):
     email: EmailStr
     password: str
     alias: Optional[str]
     rol: RolEnum
 
-# 🔹 Respuesta (lo que devolvés)
+
 class UsuarioResponse(BaseModel):
     id: int
     email: EmailStr
